1591ad316348812a86bee1406b6b4c8623ae3f69
Aaron Sultana <85006680+SirSatly@users.noreply.github.com>
2024-03-18 20:08:17 +1100

    Added documentation for 2.1P

--- Week 2/server.js
+++ Week 2/server.js
@@ -1,6 +1,13 @@
+//Imports the express npm package 
 var express = require("express")
+
+//Creates an instance of Express
 var app = express()
+
+//Defines the port for the app to run on
 var port = process.env.port || 3000;
+
+//Sets the app to listen for a request on it's port and then runs an anonymous function logging to the console when that is complete 
 app.listen(port,()=>{
     console.log("App listening to: " + port)
 })
--- Week 2/server_get.js
+++ Week 2/server_get.js
@@ -1,9 +1,14 @@
+//Imports the express npm package 
 const express= require("express");
-const res = require("express/lib/response");
+
+//Creates an instance of Express
 const app= express();
-const addTwoNumber= (n1,n2) => {
-    return n1+n2;
-}
+
+//Defines a function to add two numbers together 
+const addTwoNumber = (n1,n2) => n1+n2;
+
+//Defines a GET request at the root/addTwoNumber url which take two query parameters n1 and n2 which should both be numbers
+//These to parameters are converted from strings to ints the added together then the result then the result plus the status code 200 is outputted to the page
 app.get("/addTwoNumber", (req,res)=>{
     const n1= parseInt(req.query.n1);
     const n2=parseInt(req.query.n2);
@@ -11,12 +16,17 @@ app.get("/addTwoNumber", (req,res)=>{
     res.json({statuscocde:200, data: result });
 });
 
+//Defines a GET request at the root url to display the heading HELLO THERE in the web browser
 app.get("/", (req, res) => {
     const n1 = "<html><body><H1>HELLO THERE </H1></body></html>";
     res.set('Content-Type', 'text/html');
     res.send(Buffer.from(n1));     
 })
+
+//Logs the result of 19+12 to the console
 console.log (addTwoNumber(19,12));
+
+//Sets the webapp to use port 3040 to listen for the GET requests defined above
 const port=3040;
 app.listen(port,()=> {
     console.log("hello i'm listening to port "+port);
